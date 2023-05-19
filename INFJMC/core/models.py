30386a6c228f3be817569d77637f45acc6a771c5
javier-mansillal <javier.mansillal@usm.cl>
2023-05-19 11:41:51 -0400

    Agregado modelos.

--- INFJMC/core/models.py
+++ INFJMC/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Carrera(models.Model):
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=50)
+    duracion = models.IntegerField()
+
+class Docente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(max_length=100)
+    
